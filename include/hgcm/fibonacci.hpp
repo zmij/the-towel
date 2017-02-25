8f94901b26e6b5497e3cedf23ce06633b0ce3ccd
zmij <sergei.a.fedorov@gmail.com>
2017-02-25 16:42:47 +0300

    Fibonacci implementation variations

--- include/hgcm/fibonacci.hpp
+++ include/hgcm/fibonacci.hpp
@@ -12,6 +12,40 @@
 
 namespace hgcmp {
 
+namespace first {
+
+/**
+ * The primary metafunction template
+ */
+template < ::std::size_t N >
+struct fibonacci {
+    static constexpr ::std::size_t value =
+            fibonacci<N - 2>::value + fibonacci<N - 1>::value;
+    using type = fibonacci<N>;
+};
+
+//@{
+/** @name Terminal specializations */
+template <>
+struct fibonacci<0> {
+    static constexpr ::std::size_t value = 0;
+    using type = fibonacci<0>;
+};
+
+template <>
+struct fibonacci<1> {
+    static constexpr ::std::size_t value = 1;
+    using type = fibonacci<1>;
+};
+//@}
+} /* namespace first */
+
+
+namespace second {
+
+/**
+ * The primary metafunction template
+ */
 template < ::std::size_t N >
 struct fibonacci {
     static constexpr ::std::size_t value = fibonacci<N - 2>::value +
@@ -19,6 +53,8 @@ struct fibonacci {
     using type = ::std::integral_constant< ::std::size_t, value>;
 };
 
+//@{
+/** @name Terminal specializations */
 template <>
 struct fibonacci<0> {
     static constexpr ::std::size_t value = 0;
@@ -30,6 +66,29 @@ struct fibonacci<1> {
     static constexpr ::std::size_t value = 1;
     using type = ::std::integral_constant<::std::size_t, value>;
 };
+//@}
+} /* namespace second */
+
+inline namespace third {
+
+/**
+ * The primary metafunction template
+ */
+template < ::std::size_t N >
+struct fibonacci :
+    ::std::integral_constant<::std::size_t,
+         fibonacci<N - 2>::value + fibonacci<N - 1>::value> {};
+
+//@{
+/** @name Terminal specializations */
+template <>
+struct fibonacci<0> : ::std::integral_constant<::std::size_t, 0> {};
+
+template <>
+struct fibonacci<1> : ::std::integral_constant<::std::size_t, 1> {};
+//@}
+
+} /* namespace third */
 
 }  /* namespace hgcm */
 
